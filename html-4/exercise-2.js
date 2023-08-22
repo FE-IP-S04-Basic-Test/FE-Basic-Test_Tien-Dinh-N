f68ae28b1e533c7421906824a25c882e42cd453c
Tien Dinh N <tien.dinh@supremetech.vn>
2023-08-22 16:44:34 +0700

    Fix exercise-2 js

--- html-4/exercise-2.js
+++ html-4/exercise-2.js
@@ -13,60 +13,44 @@
   Output: { id: '1', quantity: 7 }, { id: '3', quantity: 5 }
 */
 
-const findProduct = (cartArray1, cartArray2) => {
-  // const result = cartArray1.filter((item1) => {
-  //   return !cartArray2.some((item2) => {
-  //     return item1.id === item2.id;
-  //   });
-  // });
-  // console.log(result);
-  const mergeArray = [...cartArray1, ...cartArray2];
-  const sortedArray = mergeArray.sort((a, b) => {
-    return a.id - b.id;
-  });
-
-  const idArray1 = cartArray1.map((item) => {
-    return item.id;
-  });
-
-  const idArray2 = cartArray2.map((item) => {
-    return item.id;
-  });
-
-  const sameIdArray = idArray1.filter((item) => {
-    idArray2.includes(item);
-  });
-
-  console.log(sameIdArray);
-
-  const obj = {};
-  mergeArray.forEach((item) => {
-    if (obj[item.id]) {
-      obj[item.id] += item.quantity;
+const findProduct = (arr1, arr2) => {
+  const map = arr1.reduce((mapObj, curr) => {
+    console.log(mapObj);
+    if (mapObj[curr.id]) {
+      mapObj[curr.id] += curr.quantity;
     } else {
-      obj[item.id] = item.quantity;
+      mapObj[curr.id] = curr.quantity;
     }
-  });
-
-  // cartArray2.forEach((item) => {
-  //   if (obj[item.id]) {
-  //     obj[item.id] += item.quantity;
-  //   } else {
-  //     obj[item.id] = item.quantity;
-  //   }
-  // });
-  // console.log(obj);
+    return mapObj;
+  }, {});
+  return Object.values(
+    arr2.reduce((mapObj, curr) => {
+      if (map[curr.id]) {
+        if (mapObj[curr.id]) {
+          mapObj[curr.id].quantity += curr.quantity;
+        } else {
+          mapObj[curr.id] = {
+            id: curr.id,
+            quantity: map[curr.id] + curr.quantity,
+          };
+        }
+      }
+      return mapObj;
+    }, {})
+  );
 };
 
-findProduct(
-  [
-    { id: '1', quantity: 2 },
-    { id: '2', quantity: 2 },
-    { id: '3', quantity: 3 },
-    { id: '1', quantity: 3 },
-  ],
-  [
-    { id: '1', quantity: 2 },
-    { id: '3', quantity: 2 },
-  ]
+console.log(
+  findProduct(
+    [
+      { id: '1', quantity: 2 },
+      { id: '2', quantity: 2 },
+      { id: '3', quantity: 3 },
+      { id: '1', quantity: 3 },
+    ],
+    [
+      { id: '1', quantity: 2 },
+      { id: '3', quantity: 2 },
+    ]
+  )
 );
